df27071ad901015615628b026628606b6d0aabba
Romulomiranda0901 <42875385+Romulomiranda0901@users.noreply.github.com>
2019-11-10 06:25:57 -0600

    actualizacionmapaubicacion

---
+++ MapadeUbicacion/app/src/main/java/com/example/mapadeubicacion/LocationData.java
@@ -0,0 +1,14 @@
+package com.example.mapadeubicacion;
+
+
+
+public class LocationData {
+
+    double latitude;
+    double longitude;
+
+    public LocationData(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
